b4d57212e72d9dcbe666e1855f19f71c3e209360
lidongjie <dongjie@weico.com>
2018-11-22 14:14:09 +0800

    XXX

--- SimpleToUse_UICollectionView/ViewController.m
+++ SimpleToUse_UICollectionView/ViewController.m
@@ -20,6 +20,17 @@
     [super viewDidLoad];
     
     
+    /**
+     
+        1.必须要有布局,在初始化的时候就需要. (-init{})
+        2.必须注册cell(分为代码和Xib)
+        3.UICollectionView的cell是由布局决定
+        4.自定义cell
+        // 系统自带了流水布局,专门做九宫格
+        // UICollectionViewLayout基类,UICollectionViewFlowLayout是布局对象
+     
+     */
+    
     // 创建一个layout布局类
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
     // 设置布局方向为 垂直布局
@@ -29,8 +40,22 @@
     
     // 设置每个item的大小为100*100
     layout.itemSize = CGSizeMake(100, 100);
+    // 头部尺寸
+    layout.headerReferenceSize = CGSizeMake(50, 50);
+    // 尾部尺寸
+    layout.footerReferenceSize = CGSizeMake(50, 50);
+    // 设置 垂直间距
+    layout.minimumLineSpacing = 40;
+    // 设置 水平间距
+//    layout.minimumInteritemSpacing = 50;
+    // 设置分区的头视图和尾视图是否始终固定在屏幕上边和下边
+    layout.sectionHeadersPinToVisibleBounds = YES;
+    layout.sectionFootersPinToVisibleBounds = YES;
+    
     // 创建collectionView 通过一个布局策略layout来创建
     UICollectionView *collectView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
+    // 设置 背景颜色
+    collectView.backgroundColor = [UIColor groupTableViewBackgroundColor];
     // 设置代理
     collectView.delegate = self;
     collectView.dataSource = self;
